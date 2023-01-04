cca9044b15db3207bfd3b4f812236dbe4dd7a2a1
20002911 <121868474+20002911@users.noreply.github.com>
2023-01-04 05:30:19 +0000

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace firstproject
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+        Console.WriteLine("HelloWorld");
+        }
+    }
+
+}
